b4cfcaafcd3e940f3285251cab871509250c7f1d
Abhinandan <jhaabhinandan55@gmail.com>
2024-05-18 11:07:46 +0530

    Problem 1

---
+++ Watermelons.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Watermelons{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a % 2 == 0  && a > 2){
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+        sc.close();
+    }
+}
